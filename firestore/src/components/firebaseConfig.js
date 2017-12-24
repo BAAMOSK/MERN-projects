2f370c1fb9e271a9832a17b7a45641325b3d5707
Tee <bunth667@gmail.com>
2017-12-24 10:58:12 -0500

    front end able to retrieve data

---
+++ firestore/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDjMZwYfmxOXFi2oK-wLCdjHCslUXIc5FU",
+    authDomain: "vuefs-c40c3.firebaseapp.com",
+    databaseURL: "https://vuefs-c40c3.firebaseio.com",
+    projectId: "vuefs-c40c3",
+    storageBucket: "vuefs-c40c3.appspot.com",
+    messagingSenderId: "55699578486"
+}
