33c13065bdc276991bb858fd296a5e601f8bcde0
Tee <bunth667@gmail.com>
2017-12-26 15:02:07 -0500

    wow

--- monsterHunter/app.js
+++ monsterHunter/app.js
@@ -3,7 +3,8 @@ new Vue ({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
@@ -12,35 +13,80 @@ new Vue ({
             this.monsterHealth = 100;
         },
         attack: function() {
-            this.monsterHealth -= this.calcDamage(2, 10);
-
+            let damage = this.calcDamage(2, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: `You hit monster for ${damage}.`
+            });
+            
             if(this.checkWin()) {
                 return;
             }
-
-            this.playerHealth -= this.calcDamage(3, 15);
-
-            this.checkWin();
+            this.monsterAttack();
         },
-        special: function() {},
-        heal: function() {},
-        run: function() {},
+        special: function() {
+            let damage = this.calcDamage(12, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: `BAM! You slice the beast for ${damage} damage!`
+            })
+            if(this.checkWin()) {
+                return;
+            }
+            this.monsterAttack();
+        },
+        heal: function() {
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Curaga! You heal for 10.'
+                })
+            } else {
+                this.playerHealth = 100;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: `Full recharge`
+                })
+            }
+            this.monsterAttack();
+        },
+        run: function() {
+            this.gameIsRunning = false;
+            this.turns = [];
+        },
         calcDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
+        monsterAttack: function() {
+            let damage = this.calcDamage(3, 15);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster slaps you for ${damage} damage!`
+            });
+
+            this.checkWin();
+        },
         checkWin: function() {
             if (this.monsterHealth <= 0) {
+                this.monsterHealth = 0;
                 if (confirm('You won! Play New Game?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
+                    this.turns = [];
                 }
                 return true;
             } else if (this.playerHealth <= 0) {
+                this.playerHealth = 0;
                 if (confirm('You lost! Rematch?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
+                    this.turns = [];
                 }
                 return true;
             }
