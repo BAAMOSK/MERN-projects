f4891befba1446ad269491373f64fd6cae84ec30
Tee <bunth667@gmail.com>
2017-12-26 12:03:59 -0500

    finished attack function

---
+++ monsterHunter/app.js
@@ -0,0 +1,50 @@
+new Vue ({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function() {
+            this.monsterHealth -= this.calcDamage(2, 10);
+
+            if(this.checkWin()) {
+                return;
+            }
+
+            this.playerHealth -= this.calcDamage(3, 15);
+
+            this.checkWin();
+        },
+        special: function() {},
+        heal: function() {},
+        run: function() {},
+        calcDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! Play New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! Rematch?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+});
