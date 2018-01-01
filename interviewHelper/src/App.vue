28f88a3c7ec7796e67c50acfa2324bc0a3b820bc
Tee <bunth667@gmail.com>
2018-01-01 16:18:37 -0500

    finished core quiz

---
+++ interviewHelper/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>Interview Helper</h1>
+    <component :is="mode" @answered="answered($event)" @confirmed="mode='app-question'"></component>
+  </div>
+</template>
+
+<script>
+  import Question from './components/Question'
+  import Answer from './components/Answer'
+
+  export default {
+    data () {
+      return {
+        mode: 'app-question'
+      }
+    },
+    methods: {
+      answered(isCorrect) {
+        if(isCorrect) {
+          this.mode = 'app-answer'
+        } else {
+          this.mode = 'app-question'
+          alert('Wrong!');
+        }
+      }
+    },
+    components: {
+      appQuestion: Question,
+      appAnswer: Answer
+    }
+  }
+  </script>
+
+  <style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ interviewHelper/src/components/Answer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>That's Correct!</h1>
+    <hr>
+    <button @click="onNextQuestion">Next Question</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onNextQuestion() {
+        this.$emit('confirmed');
+      }
+    }
+  }
+
+
+</script>
