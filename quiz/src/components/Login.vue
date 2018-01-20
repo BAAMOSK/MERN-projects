d2d6ad8e225d6ce02bf7e36004dd536bbbbdffc3
Tee <bunth667@gmail.com>
2018-01-19 20:15:49 -0500

    styled login page

--- quiz/src/components/Login.vue
+++ quiz/src/components/Login.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="login">
-        <h1>Register or Login</h1>
         <form>
-            <label>UserName</label>
-            <input type="text"></input>
-            <label>Password</label>
-            <input type="text"></input>
-            <div class="submit">
-                <router-link to="/dashboard">Submit</router-link>
+          <h1>Register or Login</h1>
+          <input type="text" placeholder="Name"></input>
+          <input type="text" placeholder="Password"></input>
+          <div class="submit">
+              <router-link class="btn btn--secondary" to="/dashboard">Submit</router-link>
                 <!-- <button>Submit</button> -->
-            </div>
+          </div>
         </form>
     </div>
 </template>
@@ -21,7 +19,10 @@
 
 <style scoped>
     .login {
+      height: 100vh;
         text-align: center;   
+        display: flex;
+        align-items: center;
     }
     .submit {
         margin: 25px;
@@ -33,5 +34,51 @@
         border: 2px solid black;
         border-radius: 35px;
         text-decoration: none;
+    }
+
+    .login {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      color: var(--color-primary-0);
+        background-image: linear-gradient(to right, rgba(255,0,255, .8), rgba(48,79,254 ,.9));
+    }
+
+    .login form {
+      background: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      border: 1px solid black;
+      border-radius: 10px;
+        position: relative;
+    }
+
+    .login form > * {
+      padding: 2rem;
+      font-size: 1.3rem;
+    }
+    
+    .login input {
+      margin: 50px;
+      border-radius: 10px;
+    }
+
+    .btn--secondary {
+      background: var(--color-primary-0);
+      border: 5px solid var(--color-primary-3);
+      transition: all .3s;
+      margin: 2%;
+      position: absolute;
+      top: 85%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    .btn--secondary:hover {
+      border: 10px solid var(--color-primary-3);
+      font-size: 2rem;
     }
 </style>
